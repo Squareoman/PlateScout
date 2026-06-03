dcf9868d86af2ed0d9348bedb126e7186def542a
Squareoman <jrees@engdevnet.com>
2026-06-02 17:09:35 -0700

    Add Profile page styling

--- PlateScout/src/pages/Profile.jsx
+++ PlateScout/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import "./Profile.css";
 
 function Profile() {
   const [user, setUser] = useState(null);
